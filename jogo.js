caa1591ab51e152ba4c858f96758cdbac63f059f
jroberto-inv <jroberto@inv.com.vc>
2022-04-14 01:03:03 -0300

    create first backdround use spirte and animation step one

--- jogo.js
+++ jogo.js
@@ -1,7 +1,95 @@
-console.log('Flappy Bird')
+console.log('[DevSoutinho] Flappy Bird');
 
-const stripes = new Image()
-spirts.src = './sprites.png'
+const sprites = new Image();
+sprites.src = './sprites.png';
 
-const canvas = document.querySelector('canvas')
-const contexto = canvas.getContext('2d')
+const canvas = document.querySelector('canvas');
+const contexto = canvas.getContext('2d');
+
+
+// [Background]
+const background = {
+  spriteX: 390,
+  spriteY: 0,
+  width: 275,
+  height: 204,
+  x: 0,
+  y: canvas.height - 204,
+  desenha() {
+    contexto.fillStyle = '#70c5ce';
+    contexto.fillRect(0,0, canvas.width, canvas.height)
+
+    contexto.drawImage(
+      sprites,
+      background.spriteX, background.spriteY,
+      background.width, background.height,
+      background.x, background.y,
+      background.width, background.height,
+    );
+
+    contexto.drawImage(
+      sprites,
+      background.spriteX, background.spriteY,
+      background.width, background.height,
+      (background.x + background.width), background.y,
+      background.width, background.height,
+    );
+  },
+};
+
+// [floor]
+const floor = {
+  spriteX: 0,
+  spriteY: 610,
+  width: 224,
+  height: 112,
+  x: 0,
+  y: canvas.height - 112,
+  desenha() {
+    contexto.drawImage(
+      sprites,
+      floor.spriteX, floor.spriteY,
+      floor.width, floor.height,
+      floor.x, floor.y,
+      floor.width, floor.height,
+    );
+
+    contexto.drawImage(
+      sprites,
+      floor.spriteX, floor.spriteY,
+      floor.width, floor.height,
+      (floor.x + floor.width), floor.y,
+      floor.width, floor.height,
+    );
+  },
+};
+
+const flappyBird = {
+  spriteX: 0,
+  spriteY: 0,
+  width: 33,
+  height: 24,
+  x: 10,
+  y: 50,
+  desenha() {
+    contexto.drawImage(
+      sprites,
+      flappyBird.spriteX, flappyBird.spriteY, // Sprite X, Sprite Y
+      flappyBird.width, flappyBird.height, // Tamanho do recorte na sprite
+      flappyBird.x, flappyBird.y,
+      flappyBird.width, flappyBird.height,
+    );
+  }
+}
+
+function loop() {
+  background.desenha();
+  floor.desenha();
+  flappyBird.desenha();
+
+  flappyBird.y = flappyBird.y + 1;
+
+  requestAnimationFrame(loop);
+}
+
+loop();
